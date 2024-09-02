d014d964a005d4afb2e4848196a6b624f492d5db
Lonelypactyx <lonelypactyx@yandex.ru>
2024-09-02 16:23:50 +0300

    навигация без js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.querySelector('.main-nav__toggle');
+/* document.addEventListener('DOMContentLoaded', () => {
+ const btn = document.querySelector('.main-nav__toggle--open');
   btn.classList.remove('main-nav__toggle--open');
-});
+  document.documentElement.addEventListener('resize', () => {
+    if(this.clientWidth < 768) {
+      btn.style.display = 'grid !important';
+    } else {
+      btn.classList.remove('main-nav__toggle--open');
+    }
+  });
+}); */
+const nav = document.querySelector('.main-nav__list');
+nav.insertAdjacentHTML('beforeBegin', '<button class="main-nav__toggle main-nav__toggle--open" type="button" onclick="this.classList.toggle(\'main-nav__toggle--open\')"><div class="main-nav__toggle-line"></div><span class="visually-hidden">Открыть меню</span></button>');
+nav.classList.remove('no-js');
